48095430fec514e845e49abab0260c9d36ad80f2
swimminghao1 <18709211491@163.com>
2025-12-23 15:54:35 +0800

    AddBlock创建

--- src/1day/blockchain-simple.go
+++ src/1day/blockchain-simple.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 // 1. 定义结构
 type Block struct {
@@ -16,24 +19,69 @@ type Block struct {
 func NewBlock(date string, preBlockHash []byte) *Block {
 	block := Block{
 		PreHash: preBlockHash,
-		Hash:    []byte{}, //先填空，后面再计算//TODO
+		Hash:    []byte{}, //先填空，后面再计算
 		Date:    []byte(date),
 	}
-
+	block.SetHash()
 	return &block
 }
 
-//3. ⽣成哈希
+// 3. ⽣成哈希
+func (block *Block) SetHash() {
 
-//4. 引⼊区块链
+	//	1.拼装数据
+	blockInfo := append(block.PreHash, block.Date...)
+	//	2.shah256
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
+}
+
+// 4. 引⼊区块链
+type BlockChain struct {
+	//定义一个区块链数组
+	blocks []*Block
+}
+
+// 5. 定义一个区块链
+func NewBLockChain() *BlockChain {
+	//创建一个创世块，并作为第一个区块添加到区块链中
+	genesisBlock := GenesisBlock()
+	return &BlockChain{
+		blocks: []*Block{genesisBlock},
+	}
+}
+
+// 创世块
+func GenesisBlock() *Block {
+	return NewBlock("创世块!!!!", []byte{})
+}
 
 //5. 添加区块
+
+func (bc *BlockChain) AddBlock(data string) {
+	//获取前区块的哈希
+	lastBlock := bc.blocks[len(bc.blocks)-1]
+	preHash := lastBlock.Hash
+	//	a.创建新的区块
+	block := NewBlock(data, preHash)
+	//	b.添加到区块链数组中
+	bc.blocks = append(bc.blocks, block)
+}
+
 //6. 重构代码
 
 func main() {
-	block := NewBlock("老师转班长一枚比特币！", []byte{})
-	fmt.Printf("前区块哈希值：%x\n", block.PreHash)
-	fmt.Printf("当前区块哈希值：%x\n", block.Hash)
-	fmt.Printf("区块数据：%s\n", block.Date)
-	fmt.Println("hello")
+	bc := NewBLockChain()
+	bc.AddBlock("班长向班花转了50枚比特币！")
+	bc.AddBlock("班长又向班花转了50枚比特币！")
+	bc.AddBlock("班长又向班花转了40枚比特币！")
+	for i, block := range bc.blocks {
+		fmt.Printf("=====当前区块高度：%d\n", i)
+		fmt.Printf("前区块哈希值：%x\n", block.PreHash)
+		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
+		fmt.Printf("区块数据：%s\n", block.Date)
+	}
+	//block := NewBlock("老师转班长一枚比特币！", []byte{})
+
+	//fmt.Println("hello")
 }
